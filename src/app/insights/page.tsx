8e4cc4ce0fc9e6c81311f4e27859231d26223722
Rus Madrigal <rusbenmadrigal@gmail.com>
2026-03-08 23:47:00 -0600

    Update comment in InsightsPage component to specify "Client Grid Home" for clarity.

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -99,7 +99,7 @@ export default async function InsightsPage() {
           </p>
         </section>
 
-        {/* Client Grid */}
+        {/* Client Grid Home*/}
         <InsightsIndexClient insights={insights} />
       </main>
     </div>
